8418478eb03a9dd41dbfc950e931ff86f4125697
mo92mynu <salomo.isheim@fau.de>
2022-11-27 16:01:13 +0100

    feat: created setup.py, created requirements using pip freeze, created __init__.py to make package pip installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+   name='Snowflake',
+   version='1.0.0',
+   author='Salomo Isheim',
+   url='http://pypi.python.org/pypi/PackageName/',
+   license='LICENSE.txt',
+   description='An awesome package for drawing colorful snowflakes with turtle',
+   long_description=open('README.txt').read(),
+   install_requires=[
+       "numpy >= 1.23.5"
+   ],
+)
