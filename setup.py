f76b5663c9314af15079aa527a40d7cd610b6837
mo92mynu <salomo.isheim@fau.de>
2022-11-27 16:15:22 +0100

    fix:setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
    name='Snowflake',
    version='1.0.0',
    author='Salomo Isheim',
-   url='http://pypi.python.org/pypi/PackageName/',
-   license='LICENSE.txt',
+   url='https://github.com/realLaoLong/DSSS_Let_It_snow.git',
+   license='LICENSE.md',
    description='An awesome package for drawing colorful snowflakes with turtle',
-   long_description=open('README.txt').read(),
+   long_description=open('README.md').read(),
    install_requires=[
        "numpy >= 1.23.5"
    ],
